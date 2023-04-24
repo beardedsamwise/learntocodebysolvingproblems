c285cc531b40578cf9ffc20ecde06bb26d067b03
Sam Bentley <sam.bentley@contino.io>
2023-04-24 16:19:47 +1000

    initial commit

---
+++ chapter_2/telemarketer.py
@@ -0,0 +1,16 @@
+def is_telemarketer(digit_1, digit_2, digit_3, digit_4):
+    digit_1 = int(digit_1)
+    digit_2 = int(digit_2)
+    digit_3 = int(digit_3)
+    digit_4 = int(digit_4)
+    if (8 <= digit_1 <= 9) and (digit_2 == digit_3) and (8 <= digit_4 <= 9):
+        return "ignore"
+    else:
+        return "answer"
+
+digit_1 = input()
+digit_2 = input()
+digit_3 = input()
+digit_4 = input()
+
+print(is_telemarketer(digit_1, digit_2, digit_3, digit_4))
