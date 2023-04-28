205a52028c0101ef98edb76b64cd04a19551dc38
Sam Bentley <sam.bentley@contino.io>
2023-04-28 10:03:59 +1000

    update comments

--- chapter_4/epidemiology.py
+++ chapter_4/epidemiology.py
@@ -2,13 +2,13 @@
 
 daily_infected = 0 # num of people infected daily
 day = 0 # init number of days 
-total_infected = 0 # total num of people infected 
+total_infected = 0 # init num of people infected 
 
 target_infected = int(input()) # input target number of infected people
 day_zero_infected = int(input()) # input number of people infected on day 0
 ref = int(input()) # input ref number (number of people each person infects)
 
-while total_infected <= target_infected:
+while total_infected <= target_infected: # run the loop until the number of people exceeds the target infected
     if day == 0:
         day = day + 1
         daily_infected =+ ref * day_zero_infected # number of people infected on this day
