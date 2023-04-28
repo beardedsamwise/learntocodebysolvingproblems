239069f3f42ddc6463b1360b713a82a0ca4fd782
Sam Bentley <sam.bentley@contino.io>
2023-04-28 14:23:01 +1000

    solve smarties problem :)

--- chapter_4/smarties.py
+++ chapter_4/smarties.py
@@ -1,34 +1,42 @@
 # https://dmoj.ca/problem/ecoo15r1p1
 
-# This problem goes against typical DMOJ convention and provides input for all test cases at once and I can't be bothered refactoring my code to handle this 😅
+# This problem goes against typical DMOJ convention and provides input for all test cases at once 
 
-smarties = []
+smarties_boxes = []
 
-smartie_totals = {"orange": 0, "blue" : 0, "green" : 0, "yellow" : 0, "pink" : 0, "violet" : 0, "brown" : 0, "red" : 0}
-
-time = 0
-
+# create a list of lists where the inner list is a smarties box and it's contents
+# end the loop when the end of file is reached
 while True:
-    user_input = input()
+    try:
+        temp_list = []
+        while True:
+            user_input = input()
 
-    if user_input == "end of box":
+            if user_input == "end of box":
+                break
+            else:
+                temp_list.append(user_input)
+        smarties_boxes.append(temp_list)
+    except EOFError:
         break
-    else:
-        smarties.append(user_input)
 
-for smartie in smarties:
-    smartie_totals[smartie] = smartie_totals.get(smartie) + 1
+# process the smarties in each box and print the time taken to eat each box at the end of each loop
+for smarties_box in smarties_boxes:
+    time = 0
+    smartie_totals = {"orange": 0, "blue" : 0, "green" : 0, "yellow" : 0, "pink" : 0, "violet" : 0, "brown" : 0, "red" : 0}
+    for smartie in smarties_box:
+        smartie_totals[smartie] = smartie_totals.get(smartie) + 1
 
-for smartie in smartie_totals:
-    while smartie_totals.get(smartie) > 0:
-        if not smartie == "red":
-            time = time + 13
-            smartie_totals[smartie] = smartie_totals.get(smartie) - 7
-            # print("Ate smarties of colour ", smartie, " with total now left ", smartie_totals[smartie])
-        else:
-            time = time + 16
-            smartie_totals[smartie] = smartie_totals.get(smartie) - 1
+    for smartie in smartie_totals:
+        while smartie_totals.get(smartie) > 0:
+            if not smartie == "red":
+                time = time + 13
+                smartie_totals[smartie] = smartie_totals.get(smartie) - 7
+            else:
+                time = time + 16
+                smartie_totals[smartie] = smartie_totals.get(smartie) - 1
+    print(time)
 
-print(time)
+
 
 
